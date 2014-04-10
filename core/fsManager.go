77a6115092c03aa2b4832eb62f552a9d5262444d
Matt Layher <mdlayher@gmail.com>
2014-04-10 17:01:32 -0400

    Use a safer taglib binding

--- core/fsManager.go
+++ core/fsManager.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/mdlayher/wavepipe/core/models"
 
-	"github.com/vbatts/go-taglib/taglib"
+	"github.com/wtolson/go-taglib"
 )
 
 // fsManager scans for media files in a specified path, and queues them up for inclusion
@@ -14,8 +14,12 @@ func fsManager(killFSChan chan struct{}) {
 	log.Println("fs: starting...")
 
 	// For now, this file just tests taglib
-	file := taglib.Open("/tmp/test.flac")
+	file, err := taglib.Read("/tmp/test.flac")
 	defer file.Close()
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 	// Generate a song model from the file
 	song, err := models.SongFromFile(file)
--- core/models/song.go
+++ core/models/song.go
@@ -3,14 +3,14 @@ package models
 import (
 	"errors"
 
-	"github.com/vbatts/go-taglib/taglib"
+	"github.com/wtolson/go-taglib"
 )
 
 var (
-	// ErrSongTags is returned when the tags could not be extracted from a TagLib file
-	ErrSongTags = errors.New("song: no tags could be extracted from TagLib file")
-	// ErrSongProperties is returned when the properties could not be extracted from a TagLib file
-	ErrSongProperties = errors.New("song: no properties could be extracted from TagLib file")
+	// ErrSongTags is returned when required tags could not be extracted from a TagLib file
+	ErrSongTags = errors.New("song: required tags could be extracted from TagLib file")
+	// ErrSongProperties is returned when required properties could not be extracted from a TagLib file
+	ErrSongProperties = errors.New("song: required properties could be extracted from TagLib file")
 )
 
 // Song represents a song known to wavepipe, and contains metadata regarding
@@ -24,6 +24,7 @@ type Song struct {
 	Genre      string
 	Length     int
 	SampleRate int
+	Title      string
 	Track      int
 	Year       int
 }
@@ -31,29 +32,38 @@ type Song struct {
 // SongFromFile creates a new Song from a TagLib file, extracting its tags
 // and properties to build the struct
 func SongFromFile(file *taglib.File) (*Song, error) {
-	// Retrieve tags, check for empty
-	tags := file.GetTags()
-	if tags.Title == "" && tags.Artist == "" && tags.Album == "" {
+	// Retrieve some tags needed by wavepipe, check for empty
+	// At minimum, we will need an artist and title to do anything useful with this file
+	title := file.Title()
+	artist := file.Artist()
+	if title == "" || artist == "" {
 		return nil, ErrSongTags
 	}
 
-	// Retrieve properties, check for empty
-	properties := file.GetProperties()
-	if properties.Bitrate == 0 && properties.Channels == 0 && properties.Length == 0 {
+	// Retrieve all properties, check for empty
+	// Note: length will probably be more useful as an integer, and a Duration method can
+	// be added later on if needed
+	bitrate := file.Bitrate()
+	channels := file.Channels()
+	length := int(file.Length().Seconds())
+	sampleRate := file.Samplerate()
+
+	if bitrate == 0 || channels == 0 || length == 0 || sampleRate == 0 {
 		return nil, ErrSongProperties
 	}
 
 	// Copy over fields from TagLib tags and properties
 	return &Song{
-		Album:      tags.Album,
-		Artist:     tags.Artist,
-		Bitrate:    properties.Bitrate,
-		Channels:   properties.Channels,
-		Comment:    tags.Comment,
-		Genre:      tags.Genre,
-		Length:     properties.Length,
-		SampleRate: properties.Samplerate,
-		Track:      tags.Track,
-		Year:       tags.Year,
+		Album:      file.Album(),
+		Artist:     artist,
+		Bitrate:    bitrate,
+		Channels:   channels,
+		Comment:    file.Comment(),
+		Genre:      file.Genre(),
+		Length:     length,
+		SampleRate: sampleRate,
+		Title:      title,
+		Track:      file.Track(),
+		Year:       file.Year(),
 	}, nil
 }
