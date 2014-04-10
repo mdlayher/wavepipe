d490832ad2485f7ff0d8976a9fc2fff09c32b5bb
Matt Layher <mdlayher@gmail.com>
2014-04-10 13:10:38 -0400

    Add basic doc.go

---
+++ doc.go
@@ -0,0 +1,12 @@
+/*
+Command wavepipe provides a cross-platform media server, written in Go.
+
+Installation
+
+wavepipe can be built using Go 1.1+. It can be downloaded, built, and installed,
+simply by running:
+
+	$ go get github.com/mdlayher/wavepipe
+
+*/
+package main
