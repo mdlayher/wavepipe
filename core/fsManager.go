a3afedd4ad4c34438460b00b995bdd541ea47306
Matt Layher <mdlayher@gmail.com>
2014-04-11 16:59:04 -0400

    Refactor fsManager into fsTask, fsMediaScan, fsOrphanScan, all controlled by a task queue which is cancelable on signal

--- core/fsManager.go
+++ core/fsManager.go
@@ -19,59 +19,78 @@ import (
 // which TagLib is capable of reading
 var validSet = set.New(".ape", ".flac", ".m4a", ".mp3", ".mpc", ".ogg", ".wma", ".wv")
 
+// fsTask is the interface which defines a filesystem task, such as a media scan, or an orphan scan
+type fsTask interface {
+	Scan(string, chan struct{}) error
+}
+
 // fsManager handles fsWalker processes, and communicates back and forth with the manager goroutine
 func fsManager(mediaFolder string, fsKillChan chan struct{}) {
 	log.Println("fs: starting...")
 
-	// Trigger an orphan scan, which can be halted via channel
-	orphanCancelChan := make(chan struct{})
-	orphanErrChan := fsOrphanScan(mediaFolder, orphanCancelChan)
+	// Initialize a queue of filesystem tasks
+	fsQueue := make(chan fsTask, 10)
+	cancelQueue := make(chan chan struct{}, 10)
 
-	// Trigger a filesystem walk, which can be halted via channel
-	walkCancelChan := make(chan struct{})
-	walkErrChan := fsWalker(mediaFolder, walkCancelChan)
+	// Queue an orphan scan, followed by a media scan
+	fsQueue <- new(fsOrphanScan)
+	fsQueue <- new(fsMediaScan)
 
-	// Trigger events via channel
+	// Invoke task queue via goroutine, so it can be halted via the manager
+	go func() {
+		for {
+			select {
+			// Trigger a fsTask from queue
+			case task := <-fsQueue:
+				// Create a channel to halt the scan
+				cancelChan := make(chan struct{})
+				cancelQueue <- cancelChan
+
+				// Start the scan
+				if err := task.Scan(mediaFolder, cancelChan); err != nil {
+					log.Println(err)
+				}
+
+				// On completion, close the cancel channel
+				cancelChan = <-cancelQueue
+				close(cancelChan)
+			}
+		}
+	}()
+
+	// Trigger manager events via channel
 	for {
 		select {
 		// Stop filesystem manager
 		case <-fsKillChan:
-			// Halt any in-progress walks
-			orphanCancelChan <- struct{}{}
-			walkCancelChan <- struct{}{}
+			// Halt any in-progress tasks
+			log.Println("fs: halting tasks")
+			for i := 0; i < len(cancelQueue); i++ {
+				// Receive a channel
+				f := <-cancelQueue
+				if f == nil {
+					continue
+				}
+
+				// Send termination
+				f <- struct{}{}
+				log.Println("fs: task halted")
+			}
 
 			// Inform manager that shutdown is complete
 			log.Println("fs: stopped!")
 			fsKillChan <- struct{}{}
 			return
-		// Filesystem orphan error return channel
-		case err := <-orphanErrChan:
-			// Check if error occurred
-			if err == nil {
-				break
-			}
-
-			// Report orphan errors
-			log.Println(err)
-		// Filesystem orphan error return channel
-		case err := <-walkErrChan:
-			// Check if error occurred
-			if err == nil {
-				break
-			}
-
-			// Report walk errors
-			log.Println(err)
 		}
 	}
 }
 
-// fsWalker scans for media files in a specified path, and queues them up for inclusion
-// in the wavepipe database
-func fsWalker(mediaFolder string, walkCancelChan chan struct{}) chan error {
-	// Return errors on channel
-	errChan := make(chan error)
+// fsMediaScan represents a filesystem task which scans the given path for new media
+type fsMediaScan struct{}
 
+// Scan scans for media files in a specified path, and queues them up for inclusion
+// in the wavepipe database
+func (fs *fsMediaScan) Scan(mediaFolder string, walkCancelChan chan struct{}) error {
 	// Halt walk if needed
 	var mutex sync.RWMutex
 	haltWalk := false
@@ -82,6 +101,7 @@ func fsWalker(mediaFolder string, walkCancelChan chan struct{}) chan error {
 		// Halt!
 		mutex.Lock()
 		haltWalk = true
+		log.Println("fs: halting media scan")
 		mutex.Unlock()
 	}()
 
@@ -91,121 +111,114 @@ func fsWalker(mediaFolder string, walkCancelChan chan struct{}) chan error {
 	songCount := 0
 	startTime := time.Now()
 
-	// Invoke walker goroutine
-	go func() {
-		// Invoke a recursive file walk on the given media folder, passing closure variables into
-		// walkFunc to enable additional functionality
-		log.Println("fs: beginning file walk")
-		err := filepath.Walk(mediaFolder, func(currPath string, info os.FileInfo, err error) error {
-			// Stop walking immediately if needed
-			mutex.RLock()
-			if haltWalk {
-				return errors.New("walk: halted by channel")
-			}
-			mutex.RUnlock()
+	// Invoke a recursive file walk on the given media folder, passing closure variables into
+	// walkFunc to enable additional functionality
+	log.Println("fs: beginning media scan")
+	err := filepath.Walk(mediaFolder, func(currPath string, info os.FileInfo, err error) error {
+		// Stop walking immediately if needed
+		mutex.RLock()
+		if haltWalk {
+			return errors.New("media scan: halted by channel")
+		}
+		mutex.RUnlock()
 
-			// Make sure path is actually valid
-			if info == nil {
-				return errors.New("walk: invalid path: " + currPath)
-			}
-
-			// Ignore directories for now
-			if info.IsDir() {
-				return nil
-			}
-
-			// Check for a valid media extension
-			if !validSet.Has(path.Ext(currPath)) {
-				return nil
-			}
-
-			// Attempt to scan media file with taglib
-			file, err := taglib.Read(currPath)
-			if err != nil {
-				return fmt.Errorf("%s: %s", currPath, err.Error())
-			}
-			defer file.Close()
-
-			// Generate a song model from the TagLib file, and the OS file
-			song, err := SongFromFile(file, info)
-			if err != nil {
-				return err
-			}
-
-			// Generate an artist model from this song's metadata
-			artist := ArtistFromSong(song)
-
-			// Check for existing artist
-			if err := artist.Load(); err == sql.ErrNoRows {
-				// Save new artist
-				if err := artist.Save(); err != nil {
-					log.Println(err)
-				} else if err == nil {
-					log.Printf("New artist: [%02d] %s", artist.ID, artist.Title)
-					artistCount++
-				}
-			}
-
-			// Generate the album model from this song's metadata
-			album := AlbumFromSong(song)
-			album.ArtistID = artist.ID
-
-			// Check for existing album
-			if err := album.Load(); err == sql.ErrNoRows {
-				// Save album
-				if err := album.Save(); err != nil {
-					log.Println(err)
-				} else if err == nil {
-					log.Printf("New album: [%02d] %s - %s", album.ID, album.Artist, album.Title)
-					albumCount++
-				}
-			}
-
-			// Add ID fields to song
-			song.ArtistID = artist.ID
-			song.AlbumID = album.ID
-
-			// Check for existing song
-			if err := song.Load(); err == sql.ErrNoRows {
-				// Save song
-				if err := song.Save(); err != nil {
-					log.Println(err)
-				} else if err == nil {
-					log.Printf("New song: [%02d] %s - %s - %s", song.ID, song.Artist, song.Album, song.Title)
-					songCount++
-				}
-			}
-
-			return nil
-		})
-
-		// Check for filesystem walk errors
-		if err != nil {
-			errChan <- err
-			return
+		// Make sure path is actually valid
+		if info == nil {
+			return errors.New("media scan: invalid path: " + currPath)
 		}
 
-		// Print metrics
-		log.Printf("fs: file walk complete [time: %s]", time.Since(startTime).String())
-		log.Printf("fs: [artists: %d] [albums: %d] [songs: %d]", artistCount, albumCount, songCount)
+		// Ignore directories for now
+		if info.IsDir() {
+			return nil
+		}
 
-		// No errors
-		errChan <- nil
-	}()
+		// Check for a valid media extension
+		if !validSet.Has(path.Ext(currPath)) {
+			return nil
+		}
 
-	// Return communication channel
-	return errChan
+		// Attempt to scan media file with taglib
+		file, err := taglib.Read(currPath)
+		if err != nil {
+			return fmt.Errorf("%s: %s", currPath, err.Error())
+		}
+		defer file.Close()
+
+		// Generate a song model from the TagLib file, and the OS file
+		song, err := SongFromFile(file, info)
+		if err != nil {
+			return err
+		}
+
+		// Generate an artist model from this song's metadata
+		artist := ArtistFromSong(song)
+
+		// Check for existing artist
+		if err := artist.Load(); err == sql.ErrNoRows {
+			// Save new artist
+			if err := artist.Save(); err != nil {
+				log.Println(err)
+			} else if err == nil {
+				log.Printf("Artist: [%04d] %s", artist.ID, artist.Title)
+				artistCount++
+			}
+		}
+
+		// Generate the album model from this song's metadata
+		album := AlbumFromSong(song)
+		album.ArtistID = artist.ID
+
+		// Check for existing album
+		if err := album.Load(); err == sql.ErrNoRows {
+			// Save album
+			if err := album.Save(); err != nil {
+				log.Println(err)
+			} else if err == nil {
+				log.Printf("  - Album: [%04d] %s - %d - %s", album.ID, album.Artist, album.Year, album.Title)
+				albumCount++
+			}
+		}
+
+		// Add ID fields to song
+		song.ArtistID = artist.ID
+		song.AlbumID = album.ID
+
+		// Check for existing song
+		if err := song.Load(); err == sql.ErrNoRows {
+			// Save song (don't log these because they really slow things down)
+			if err := song.Save(); err != nil {
+				log.Println(err)
+			} else if err == nil {
+				songCount++
+			}
+		}
+
+		// Successful media scan
+		return nil
+	})
+
+	// Check for filesystem walk errors
+	if err != nil {
+		return err
+	}
+
+	// Print metrics
+	log.Printf("fs: media scan complete [time: %s]", time.Since(startTime).String())
+	log.Printf("fs: [artists: %d] [albums: %d] [songs: %d]", artistCount, albumCount, songCount)
+
+	// No errors
+	return nil
 }
 
-// fsOrphanScan scans for media files which have been removed from the media directory, and removes
+// fsOrphanScan represents a filesystem task which scans the given path for orphaned media
+type fsOrphanScan struct{}
+
+// Scan scans for media files which have been removed from the media directory, and removes
 // them as appropriate.  An orphan is defined as follows:
 //   - Artist: no more songs contain this artist's ID
 //   - Album: no more songs contain this album's ID
 //   - Song: song is no longer present in the filesystem
-func fsOrphanScan(mediaFolder string, orphanCancelChan chan struct{}) chan error {
-	// Return errors on channel
-	errChan := make(chan error)
-
+func (fs *fsOrphanScan) Scan(mediaFolder string, orphanCancelChan chan struct{}) error {
 	// Halt scan if needed
 	var mutex sync.RWMutex
 	haltOrphanScan := false
@@ -215,6 +228,7 @@ func fsOrphanScan(mediaFolder string, orphanCancelChan chan struct{}) chan error
 
 		// Halt!
 		mutex.Lock()
+		log.Println("fs: halting orphan scan")
 		haltOrphanScan = true
 		mutex.Unlock()
 	}()
@@ -225,18 +239,12 @@ func fsOrphanScan(mediaFolder string, orphanCancelChan chan struct{}) chan error
 	songCount := 0
 	startTime := time.Now()
 
-	// Invoke scanner goroutine
-	go func() {
-		log.Println("fs: beginning orphan scan")
+	log.Println("fs: beginning orphan scan")
 
-		// Print metrics
-		log.Printf("fs: orphan scan complete [time: %s]", time.Since(startTime).String())
-		log.Printf("fs: [artists: %d] [albums: %d] [songs: %d]", artistCount, albumCount, songCount)
+	// Print metrics
+	log.Printf("fs: orphan scan complete [time: %s]", time.Since(startTime).String())
+	log.Printf("fs: [artists: %d] [albums: %d] [songs: %d]", artistCount, albumCount, songCount)
 
-		// No errors
-		errChan <- nil
-	}()
-
-	// Return communication channel
-	return errChan
+	// No errors
+	return nil
 }
