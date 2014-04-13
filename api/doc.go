41e36441eaebb76cf3a4d6a74cc808ccb16c2650
Matt Layher <mdlayher@gmail.com>
2014-04-13 16:01:45 -0400

    Add doc.go for api and data packages

---
+++ api/doc.go
@@ -0,0 +1,4 @@
+/*
+Package api provides the RESTful API of the wavepipe media server.
+*/
+package api
---
+++ data/doc.go
@@ -0,0 +1,4 @@
+/*
+Package data provides the models and database backends of the wavepipe media server.
+*/
+package data
