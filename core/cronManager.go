2ba0f0ab22237324b8b05ec63b97ad61d1cfc8b4
Matt Layher <mdlayher@gmail.com>
2014-04-15 19:18:41 -0400

    Add orphan scan to cron every 30 minutes

--- core/cronManager.go
+++ core/cronManager.go
@@ -3,15 +3,26 @@ package core
 import (
 	"log"
 	"time"
+
+	"github.com/mdlayher/wavepipe/config"
 )
 
 // cronManager spawns and triggers events at regular intervals
 func cronManager(cronKillChan chan struct{}) {
 	log.Println("cron: starting...")
 
+	// Retrieve configuration for use with crons
+	conf, err := config.C.Load()
+	if err != nil {
+		log.Fatal("cron: could not load configuration")
+	}
+
 	// cronPrintCurrentStatus - run on startup, and every 5 minutes
 	status := time.NewTicker(5 * time.Minute)
 	go cronPrintCurrentStatus()
+
+	// cronOrphanScan - scan every 30 minutes
+	orphanScan := time.NewTicker(30 * time.Minute)
 
 	// Trigger events via ticker
 	for {
@@ -22,8 +33,15 @@ func cronManager(cronKillChan chan struct{}) {
 			log.Println("cron: stopped!")
 			cronKillChan <- struct{}{}
 			return
+		// Trigger status printing
 		case <-status.C:
 			go cronPrintCurrentStatus()
+		// Trigger orphan scan
+		case <- orphanScan.C:
+			// Queue a new orphan scan
+			o := new(fsOrphanScan)
+			o.SetFolders(conf.Media(), "")
+			fsQueue <- o
 		}
 	}
 }
