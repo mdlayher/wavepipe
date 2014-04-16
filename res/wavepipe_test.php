53fd4d7a287ba375d543f35ecfc09d19906d21b9
Matt Layher <mdlayher@gmail.com>
2014-04-16 02:20:57 -0400

    Split PHP functions into common file

--- res/wavepipe_test.php
+++ res/wavepipe_test.php
@@ -1,24 +1,6 @@
 <?php
 
-// apiSignature creates a HMAC-SHA1 API signature
-function apiSignature($public, $nonce, $method, $resource, $secret) {
-	// Create API signature string
-	$signString = sprintf("%s-%s-%s-%s", $public, $nonce, $method, $resource);
-
-	// Return HMAC-SHA1 signature
-	return hash_hmac("sha1", $signString, $secret);
-}
-
-// generateNonce creates a nonce value for use with the API
-function generateNonce($length = 10) {
-	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	$randomString = '';
-	for ($i = 0; $i < $length; $i++) {
-		$randomString .= $characters[rand(0, strlen($characters) - 1)];
-	}
-
-	return $randomString;
-}
+require_once __DIR__ . "/wavepipe.php";
 
 // Attempt a login request using test credentials
 $login = json_decode(file_get_contents("http://localhost:8080/api/v0/login?u=test&p=test"), true);
