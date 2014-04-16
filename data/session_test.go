235ae0c2ee06c9ba756085faf25139cba95f9ae3
Matt Layher <mdlayher@gmail.com>
2014-04-15 22:25:08 -0400

    Add data/session_test

---
+++ data/session_test.go
@@ -0,0 +1,49 @@
+package data
+
+import (
+	"testing"
+)
+
+// TestSessionDatabase verifies that an Session can be saved and loaded from the database
+func TestSessionDatabase(t *testing.T) {
+	// Load database configuration
+	DB = new(SqliteBackend)
+	DB.DSN("~/.config/wavepipe/wavepipe.db")
+
+	// Attempt to create and save the session
+	session, err := NewSession(1, "TestPassword", "TestClient")
+	if err != nil {
+		t.Fatalf("Could not create and save session: %s", err.Error())
+	}
+
+	// Attempt to load the session
+	if err := session.Load(); err != nil {
+		t.Fatalf("Could not load session: %s", err.Error())
+	}
+
+	// Attempt to delete the session
+	if err := session.Delete(); err != nil {
+		t.Fatalf("Could not delete session: %s", err.Error())
+	}
+}
+
+// TestSessionJSON verifies that an Session can be encoded and decoded from JSON
+func TestSessionJSON(t *testing.T) {
+	// Mock session
+	session := new(Session)
+	session.UserID = 1
+	session.Client = "TestClient"
+	session.PublicKey = "TestPublicKey"
+	session.SecretKey = "TestSecretKey"
+
+	// Marshal JSON
+	out, err := session.ToJSON()
+	if err != nil {
+		t.Fatalf("Could not encode JSON: %s", err.Error())
+	}
+
+	// Unmarshal
+	if err := session.FromJSON(out); err != nil {
+		t.Fatalf("Could not decode JSON: %s", err.Error())
+	}
+}
