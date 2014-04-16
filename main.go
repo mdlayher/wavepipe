2ba26c997c2259aaaf2dd6268c0cddb66d40b570
Matt Layher <mdlayher@gmail.com>
2014-04-16 17:01:58 -0400

    Increase test mode time

--- main.go
+++ main.go
@@ -48,8 +48,8 @@ func main() {
 		}
 
 		go func() {
-			// Wait 10 seconds, to allow reasonable startup time
-			seconds := 10
+			// Wait 30 seconds, to allow reasonable startup time
+			seconds := 30
 			log.Println(core.App, ": started in test mode, stopping in", seconds, "seconds.")
 			<-time.After(time.Duration(seconds) * time.Second)
 
