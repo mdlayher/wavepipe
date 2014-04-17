42adfa8aadb3ac8217411263930a1103cab85c7c
Matt Layher <mdlayher@gmail.com>
2014-04-17 15:17:05 -0400

    api/logout, add shortcut methods for rendering errors and server errors

--- api/logout.go
+++ api/logout.go
@@ -13,21 +13,36 @@ import (
 // LogoutResponse represents the JSON response for /api/logouts
 type LogoutResponse struct {
 	Error *Error `json:"error"`
+	render  render.Render `json:"-"`
+}
+
+// RenderError renders a JSON error message with the specified HTTP status code and message
+func (l *LogoutResponse) RenderError(code int, message string) {
+	// Generate error
+	l.Error = new(Error)
+	l.Error.Code = code
+	l.Error.Message = message
+
+	// Render with specified HTTP status code
+	l.render.JSON(code, l)
+}
+
+// ServerError is a shortcut to render a HTTP 500 with generic "server error" message
+func (l *LogoutResponse) ServerError() {
+	l.RenderError(500, "server error")
+	return
 }
 
 // GetLogout destroys a new session from the wavepipe API, and returns a HTTP status and JSON
 func GetLogout(r render.Render, req *http.Request, session *data.Session, params martini.Params) {
 	// Output struct for logouts request
-	res := LogoutResponse{}
+	res := LogoutResponse{render: r}
 
 	// Check API version
 	if version, ok := params["version"]; ok {
 		// Check if this API call is supported in the advertised version
 		if !apiVersionSet.Has(version) {
-			res.Error = new(Error)
-			res.Error.Code = 400
-			res.Error.Message = "unsupported API version: " + version
-			r.JSON(400, res)
+			res.RenderError(400, "unsupported API version: "+version)
 			return
 		}
 	}
@@ -35,11 +50,7 @@ func GetLogout(r render.Render, req *http.Request, session *data.Session, params
 	// Destroy the current API session
 	if err := session.Delete(); err != nil {
 		log.Println(err)
-
-		res.Error = new(Error)
-		res.Error.Code = 500
-		res.Error.Message = "server error"
-		r.JSON(500, res)
+		res.ServerError()
 		return
 	}
 
