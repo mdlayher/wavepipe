b19dc050e5c6c78f4a5cc6264234aab7989ac9c3
Matt Layher <mdlayher@gmail.com>
2014-04-17 20:53:18 -0400

    Add more necessary methods to Options interface

--- transcode/options.go
+++ transcode/options.go
@@ -1,14 +1,13 @@
 package transcode
 
-import (
-
-)
-
 // Options represents an audio codec and its quality settings, and includes methods to
 // retrieve these settings
 type Options interface {
 	Codec() string
+	Ext() string
 	FFmpegCodec() string
+	FFmpegFlags() string
 	FFmpegQuality() string
+	MIMEType() string
 	Quality() string
 }
