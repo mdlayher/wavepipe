d09c5794141687a4d8c8d1af0352b05376806313
Matt Layher <mdlayher@gmail.com>
2014-04-20 23:30:45 -0400

    Initial commit of art resize functionality

--- api/art.go
+++ api/art.go
@@ -1,7 +1,11 @@
 package api
 
 import (
+	"bytes"
 	"database/sql"
+	"image"
+	_ "image/jpeg"
+	"image/png"
 	"io"
 	"io/ioutil"
 	"log"
@@ -16,11 +20,12 @@ import (
 
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
+	"github.com/nfnt/resize"
 )
 
 // GetArt a binary art file from wavepipe.  On success, this API will
 // return binary art. On failure, it will return a JSON error.
-func GetArt(httpRes http.ResponseWriter, r render.Render, params martini.Params) {
+func GetArt(httpReq *http.Request, httpRes http.ResponseWriter, r render.Render, params martini.Params) {
 	// Output struct for art errors
 	res := ErrorResponse{render: r}
 
@@ -72,21 +77,79 @@ func GetArt(httpRes http.ResponseWriter, r render.Render, params martini.Params)
 	}
 	defer stream.Close()
 
-	// Read in the entire art stream
-	artBuf, err := ioutil.ReadAll(stream)
-	if err != nil {
-		log.Println(err)
-		res.ServerError()
-		return
+	// Output for HTTP headers
+	var length int64
+	var mimeType string
+
+	// Output art buffer
+	artBuf := make([]byte, 0)
+
+	// Check for resize request
+	size := httpReq.URL.Query().Get("size")
+	if size != "" {
+		// Ensure size is a positive integer
+		sizeInt, err := strconv.Atoi(size)
+		if err != nil {
+			log.Println(err)
+			res.ServerError()
+			return
+		}
+
+		// Verify positive integer
+		if sizeInt < 1 {
+			res.RenderError(400, "negative integer size")
+			return
+		}
+
+		// Decode input image stream
+		img, _, err := image.Decode(stream)
+		if err != nil {
+			log.Println(err)
+			res.ServerError()
+			return
+		}
+
+		// Generate a thumbnail image of the specified size
+		thumb := resize.Resize(uint(sizeInt), 0, img, resize.NearestNeighbor)
+
+		// Encode image as PNG to prevent further quality loss
+		buffer := bytes.NewBuffer(make([]byte, 0))
+		if err := png.Encode(buffer, thumb); err != nil {
+			log.Println(err)
+			res.ServerError()
+			return
+		}
+
+		// Copy PNG into output buffer
+		artBuf = buffer.Bytes()
+
+		// Set HTTP response
+		length = int64(buffer.Len())
+		mimeType = "image/png"
+	} else {
+		// Read in the entire art stream
+		tempBuf, err := ioutil.ReadAll(stream)
+		if err != nil {
+			log.Println(err)
+			res.ServerError()
+			return
+		}
+
+		// Copy into output buffer
+		artBuf = tempBuf
+
+		// Set HTTP response
+		length = art.FileSize
+		mimeType = mime.TypeByExtension(path.Ext(art.FileName))
 	}
 
 	// Set necessary HTTP output headers
 
 	// Get content length from file size
-	httpRes.Header().Set("Content-Length", strconv.FormatInt(art.FileSize, 10))
+	httpRes.Header().Set("Content-Length", strconv.FormatInt(length, 10))
 
 	// Set content type via MIME type
-	httpRes.Header().Set("Content-Type", mime.TypeByExtension(path.Ext(art.FileName)))
+	httpRes.Header().Set("Content-Type", mimeType)
 
 	// Get art last modify time in RFC1123 format, replace UTC with GMT
 	lastMod := strings.Replace(time.Unix(art.LastModified, 0).UTC().Format(time.RFC1123), "UTC", "GMT", 1)
