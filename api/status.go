212b199febdd2cde8a53de5ed2e0275f46b8eb4f
Matt Layher <mdlayher@gmail.com>
2014-06-09 11:42:07 -0400

    api/status: move Metrics to common/metrics

--- api/status.go
+++ api/status.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/mdlayher/wavepipe/common"
-	"github.com/mdlayher/wavepipe/data"
 
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
@@ -13,17 +12,9 @@ import (
 
 // StatusResponse represents the JSON response for /api/status
 type StatusResponse struct {
-	Error   *Error         `json:"error"`
-	Status  *common.Status `json:"status"`
-	Metrics *Metrics       `json:"metrics"`
-}
-
-// Metrics represents a variety of metrics about wavepipe's database
-type Metrics struct {
-	Artists int64 `json:"artists"`
-	Albums  int64 `json:"albums"`
-	Songs   int64 `json:"songs"`
-	Folders int64 `json:"folders"`
+	Error   *Error          `json:"error"`
+	Status  *common.Status  `json:"status"`
+	Metrics *common.Metrics `json:"metrics"`
 }
 
 // GetStatus returns the current server status, with an HTTP status and JSON
@@ -56,45 +47,15 @@ func GetStatus(req *http.Request, r render.Render, params martini.Params) {
 
 	// If requested, fetch additional metrics (not added by default due to full table scans in database)
 	if pMetrics := req.URL.Query().Get("metrics"); pMetrics == "true" {
-		// Fetch total artists
-		artists, err := data.DB.CountArtists()
+		metrics, err := common.ServerMetrics()
 		if err != nil {
 			log.Println(err)
 			errRes.ServerError()
 			return
 		}
 
-		// Fetch total albums
-		albums, err := data.DB.CountAlbums()
-		if err != nil {
-			log.Println(err)
-			errRes.ServerError()
-			return
-		}
-
-		// Fetch total songs
-		songs, err := data.DB.CountSongs()
-		if err != nil {
-			log.Println(err)
-			errRes.ServerError()
-			return
-		}
-
-		// Fetch total folders
-		folders, err := data.DB.CountFolders()
-		if err != nil {
-			log.Println(err)
-			errRes.ServerError()
-			return
-		}
-
-		// Add metrics to output
-		res.Metrics = &Metrics{
-			Artists: artists,
-			Albums:  albums,
-			Songs:   songs,
-			Folders: folders,
-		}
+		// Return metrics
+		res.Metrics = metrics
 	}
 
 	// HTTP 200 OK with JSON
