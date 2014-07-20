7ebc326ca836ff7a42e72a48d2c22f78378eff69
Matt Layher <mdlayher@gmail.com>
2014-07-20 11:35:34 -0400

    api/artists: refactor and cleanup

--- api/artists.go
+++ api/artists.go
@@ -14,7 +14,7 @@ import (
 	"github.com/unrolled/render"
 )
 
-// ArtistsResponse represents the JSON response for /api/artists
+// ArtistsResponse represents the JSON response for the Artists API.
 type ArtistsResponse struct {
 	Error   *Error        `json:"error"`
 	Artists []data.Artist `json:"artists"`
@@ -22,56 +22,57 @@ type ArtistsResponse struct {
 	Songs   []data.Song   `json:"songs"`
 }
 
-// GetArtists retrieves one or more artists from wavepipe, and returns a HTTP status and JSON
-func GetArtists(res http.ResponseWriter, req *http.Request) {
+// GetArtists retrieves one or more artists from wavepipe, and returns a HTTP status and JSON.
+// It can be used to fetch a single artist, a limited subset of artists, or all artists, depending
+// on the request parameters.
+func GetArtists(w http.ResponseWriter, r *http.Request) {
 	// Retrieve render
-	r := context.Get(req, CtxRender).(*render.Render)
+	ren := context.Get(r, CtxRender).(*render.Render)
 
 	// Output struct for artists request
 	out := ArtistsResponse{}
 
-	// List of artists to return
-	artists := make([]data.Artist, 0)
-
 	// Check API version
-	if version, ok := mux.Vars(req)["version"]; ok {
+	if version, ok := mux.Vars(r)["version"]; ok {
 		// Check if this API call is supported in the advertised version
 		if !apiVersionSet.Has(version) {
-			r.JSON(res, 400, errRes(400, "unsupported API version: "+version))
+			ren.JSON(w, 400, errRes(400, "unsupported API version: "+version))
 			return
 		}
 	}
 
 	// Check for an ID parameter
-	if pID, ok := mux.Vars(req)["id"]; ok {
+	if pID, ok := mux.Vars(r)["id"]; ok {
 		// Verify valid integer ID
 		id, err := strconv.Atoi(pID)
 		if err != nil {
-			r.JSON(res, 400, errRes(400, "invalid integer artist ID"))
+			ren.JSON(w, 400, errRes(400, "invalid integer artist ID"))
 			return
 		}
 
 		// Load the artist
-		artist := new(data.Artist)
-		artist.ID = id
+		artist := &data.Artist{ID: id}
 		if err := artist.Load(); err != nil {
 			// Check for invalid ID
 			if err == sql.ErrNoRows {
-				r.JSON(res, 404, errRes(404, "artist ID not found"))
+				ren.JSON(w, 404, errRes(404, "artist ID not found"))
 				return
 			}
 
 			// All other errors
 			log.Println(err)
-			r.JSON(res, 500, serverErr)
+			ren.JSON(w, 500, serverErr)
 			return
 		}
+
+		// Store artist in slice
+		out.Artists = []data.Artist{*artist}
 
 		// On single artist, load the albums for this artist
 		albums, err := data.DB.AlbumsForArtist(artist.ID)
 		if err != nil {
 			log.Println(err)
-			r.JSON(res, 500, serverErr)
+			ren.JSON(w, 500, serverErr)
 			return
 		}
 
@@ -79,12 +80,12 @@ func GetArtists(res http.ResponseWriter, req *http.Request) {
 		out.Albums = albums
 
 		// Get songs for this artist if parameter is true
-		if req.URL.Query().Get("songs") == "true" {
+		if r.URL.Query().Get("songs") == "true" {
 			// If requested, on single artist, load the songs for this artist
 			songs, err := data.DB.SongsForArtist(artist.ID)
 			if err != nil {
 				log.Println(err)
-				r.JSON(res, 500, serverErr)
+				ren.JSON(w, 500, serverErr)
 				return
 			}
 
@@ -92,48 +93,45 @@ func GetArtists(res http.ResponseWriter, req *http.Request) {
 			out.Songs = songs
 		}
 
-		// Add artist to slice
-		artists = append(artists, *artist)
-	} else {
-		// Check for a limit parameter
-		if pLimit := req.URL.Query().Get("limit"); pLimit != "" {
-			// Split limit into two integers
-			var offset int
-			var count int
-			if n, err := fmt.Sscanf(pLimit, "%d,%d", &offset, &count); n < 2 || err != nil {
-				r.JSON(res, 400, errRes(400, "invalid comma-separated integer pair for limit"))
-				return
-			}
-
-			// Retrieve limited subset of artists
-			tempArtists, err := data.DB.LimitArtists(offset, count)
-			if err != nil {
-				log.Println(err)
-				r.JSON(res, 500, serverErr)
-				return
-			}
-
-			// Copy artists into the output slice
-			artists = tempArtists
-		} else {
-			// Retrieve all artists
-			tempArtists, err := data.DB.AllArtists()
-			if err != nil {
-				log.Println(err)
-				r.JSON(res, 500, serverErr)
-				return
-			}
-
-			// Copy artists into the output slice
-			artists = tempArtists
-		}
+		// HTTP 200 OK with JSON
+		ren.JSON(w, 200, out)
+		return
 	}
 
-	// Build response
-	out.Error = nil
-	out.Artists = artists
+	// Check for a limit parameter
+	if pLimit := r.URL.Query().Get("limit"); pLimit != "" {
+		// Split limit into two integers
+		var offset int
+		var count int
+		if n, err := fmt.Sscanf(pLimit, "%d,%d", &offset, &count); n < 2 || err != nil {
+			ren.JSON(w, 400, errRes(400, "invalid comma-separated integer pair for limit"))
+			return
+		}
+
+		// Retrieve limited subset of artists
+		artists, err := data.DB.LimitArtists(offset, count)
+		if err != nil {
+			log.Println(err)
+			ren.JSON(w, 500, serverErr)
+			return
+		}
+
+		// HTTP 200 OK with JSON
+		out.Artists = artists
+		ren.JSON(w, 200, out)
+		return
+	}
+
+	// If no other case, retrieve all artists
+	artists, err := data.DB.AllArtists()
+	if err != nil {
+		log.Println(err)
+		ren.JSON(w, 500, serverErr)
+		return
+	}
 
 	// HTTP 200 OK with JSON
-	r.JSON(res, 200, out)
+	out.Artists = artists
+	ren.JSON(w, 200, out)
 	return
 }
