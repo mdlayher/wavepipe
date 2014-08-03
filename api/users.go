e329c92f04cfcc2eec448fb9a1ad49a82e832408
Matt Layher <mdlayher@gmail.com>
2014-08-03 15:14:43 -0400

    api/users: refactor and cleanup

--- api/users.go
+++ api/users.go
@@ -13,94 +13,87 @@ import (
 	"github.com/unrolled/render"
 )
 
-// UsersResponse represents the JSON response for /api/users
+// UsersResponse represents the JSON response for the Users API.
 type UsersResponse struct {
 	Error *Error      `json:"error"`
 	Users []data.User `json:"users"`
 }
 
-// GetUsers retrieves one or more users from wavepipe, and returns a HTTP status and JSON
-func GetUsers(res http.ResponseWriter, req *http.Request) {
+// GetUsers retrieves one or more users from wavepipe, and returns a HTTP status and JSON.
+// It can be used to fetch a single user, or all users, depending on the request parameters.
+func GetUsers(w http.ResponseWriter, r *http.Request) {
 	// Retrieve render
-	r := context.Get(req, CtxRender).(*render.Render)
+	ren := context.Get(r, CtxRender).(*render.Render)
 
 	// Output struct for users request
 	out := UsersResponse{}
 
-	// List of users to return
-	users := make([]data.User, 0)
-
 	// Check API version
-	if version, ok := mux.Vars(req)["version"]; ok {
+	if version, ok := mux.Vars(r)["version"]; ok {
 		// Check if this API call is supported in the advertised version
 		if !apiVersionSet.Has(version) {
-			r.JSON(res, 400, errRes(400, "unsupported API version: "+version))
+			ren.JSON(w, 400, errRes(400, "unsupported API version: "+version))
 			return
 		}
 	}
 
 	// Check for an ID parameter
-	if pID, ok := mux.Vars(req)["id"]; ok {
+	if pID, ok := mux.Vars(r)["id"]; ok {
 		// Verify valid integer ID
 		id, err := strconv.Atoi(pID)
 		if err != nil {
-			r.JSON(res, 400, errRes(400, "invalid integer user ID"))
+			ren.JSON(w, 400, errRes(400, "invalid integer user ID"))
 			return
 		}
 
 		// Load the user
-		user := new(data.User)
-		user.ID = id
+		user := &data.User{ID: id}
 		if err := user.Load(); err != nil {
 			// Check for invalid ID
 			if err == sql.ErrNoRows {
-				r.JSON(res, 404, errRes(404, "user ID not found"))
+				ren.JSON(w, 404, errRes(404, "user ID not found"))
 				return
 			}
 
 			// All other errors
 			log.Println(err)
-			r.JSON(res, 500, serverErr)
+			ren.JSON(w, 500, serverErr)
 			return
 		}
 
-		// Add user to slice
-		users = append(users, *user)
-	} else {
-		// Retrieve all users
-		tempUsers, err := data.DB.AllUsers()
-		if err != nil {
-			log.Println(err)
-			r.JSON(res, 500, serverErr)
-			return
-		}
-
-		// Copy users into the output slice
-		users = tempUsers
+		// HTTP 200 OK with JSON
+		out.Users = []data.User{*user}
+		ren.JSON(w, 200, out)
+		return
 	}
 
-	// Build response
-	out.Error = nil
-	out.Users = users
+	// If no other case, retrieve all users
+	users, err := data.DB.AllUsers()
+	if err != nil {
+		log.Println(err)
+		ren.JSON(w, 500, serverErr)
+		return
+	}
 
 	// HTTP 200 OK with JSON
-	r.JSON(res, 200, out)
+	out.Users = users
+	ren.JSON(w, 200, out)
 	return
 }
 
-// PostUsers creates a new user on the wavepipe API, and returns a HTTP status and JSON
-func PostUsers(res http.ResponseWriter, req *http.Request) {
+// PostUsers creates a new user for the wavepipe API, and returns a HTTP status and JSON.
+func PostUsers(w http.ResponseWriter, r *http.Request) {
 	// Retrieve render
-	r := context.Get(req, CtxRender).(*render.Render)
+	ren := context.Get(r, CtxRender).(*render.Render)
 
 	// Attempt to retrieve user from context
 	sessionUser := new(data.User)
-	if tempUser := context.Get(req, CtxUser); tempUser != nil {
+	if tempUser := context.Get(r, CtxUser); tempUser != nil {
 		sessionUser = tempUser.(*data.User)
 	} else {
 		// No sessionUser stored in context
 		log.Println("api: no sessionUser stored in request context!")
-		r.JSON(res, 500, serverErr)
+		ren.JSON(w, 500, serverErr)
 		return
 	}
 
@@ -108,44 +101,44 @@ func PostUsers(res http.ResponseWriter, req *http.Request) {
 	out := UsersResponse{}
 
 	// Check API version
-	if version, ok := mux.Vars(req)["version"]; ok {
+	if version, ok := mux.Vars(r)["version"]; ok {
 		// Check if this API call is supported in the advertised version
 		if !apiVersionSet.Has(version) {
-			r.JSON(res, 400, errRes(400, "unsupported API version: "+version))
+			ren.JSON(w, 400, errRes(400, "unsupported API version: "+version))
 			return
 		}
 	}
 
 	// Only allow administrators to create users
 	if sessionUser.RoleID < data.RoleAdmin {
-		r.JSON(res, 403, permissionErr)
+		ren.JSON(w, 403, permissionErr)
 		return
 	}
 
 	// Check for required username, password, and role parameters
-	username := req.PostFormValue("username")
+	username := r.PostFormValue("username")
 	if username == "" {
-		r.JSON(res, 400, errRes(400, "missing required parameter: username"))
+		ren.JSON(w, 400, errRes(400, "missing required parameter: username"))
 		return
 	}
 
-	password := req.PostFormValue("password")
+	password := r.PostFormValue("password")
 	if password == "" {
-		r.JSON(res, 400, errRes(400, "missing required parameter: password"))
+		ren.JSON(w, 400, errRes(400, "missing required parameter: password"))
 		return
 	}
 
 	// Check for role ID
-	role := req.PostFormValue("role")
+	role := r.PostFormValue("role")
 	if role == "" {
-		r.JSON(res, 400, errRes(400, "missing required parameter: role"))
+		ren.JSON(w, 400, errRes(400, "missing required parameter: role"))
 		return
 	}
 
 	// Ensure role is valid integer, and valid role
 	roleID, err := strconv.Atoi(role)
 	if err != nil || (roleID != data.RoleGuest && roleID != data.RoleUser && roleID != data.RoleAdmin) {
-		r.JSON(res, 400, errRes(400, "invalid integer role ID"))
+		ren.JSON(w, 400, errRes(400, "invalid integer role ID"))
 		return
 	}
 
@@ -153,32 +146,29 @@ func PostUsers(res http.ResponseWriter, req *http.Request) {
 	user, err := data.NewUser(username, password, roleID)
 	if err != nil {
 		log.Println(err)
-		r.JSON(res, 500, serverErr)
+		ren.JSON(w, 500, serverErr)
 		return
 	}
 
-	// Build response
-	out.Users = append(out.Users, *user)
-	out.Error = nil
-
 	// HTTP 200 OK with JSON
-	r.JSON(res, 200, out)
+	out.Users = []data.User{*user}
+	ren.JSON(w, 200, out)
 	return
 }
 
-// PutUsers updates users on wavepipe, and returns a HTTP status and JSON
-func PutUsers(res http.ResponseWriter, req *http.Request) {
+// PutUsers updates users on the wavepipe API, and returns a HTTP status and JSON.
+func PutUsers(w http.ResponseWriter, r *http.Request) {
 	// Retrieve render
-	r := context.Get(req, CtxRender).(*render.Render)
+	ren := context.Get(r, CtxRender).(*render.Render)
 
 	// Attempt to retrieve user from context
 	sessionUser := new(data.User)
-	if tempUser := context.Get(req, CtxUser); tempUser != nil {
+	if tempUser := context.Get(r, CtxUser); tempUser != nil {
 		sessionUser = tempUser.(*data.User)
 	} else {
 		// No sessionUser stored in context
 		log.Println("api: no sessionUser stored in request context!")
-		r.JSON(res, 500, serverErr)
+		ren.JSON(w, 500, serverErr)
 		return
 	}
 
@@ -186,111 +176,107 @@ func PutUsers(res http.ResponseWriter, req *http.Request) {
 	out := UsersResponse{}
 
 	// Check API version
-	if version, ok := mux.Vars(req)["version"]; ok {
+	if version, ok := mux.Vars(r)["version"]; ok {
 		// Check if this API call is supported in the advertised version
 		if !apiVersionSet.Has(version) {
-			r.JSON(res, 400, errRes(400, "unsupported API version: "+version))
+			ren.JSON(w, 400, errRes(400, "unsupported API version: "+version))
 			return
 		}
 	}
 
 	// Disallow guests from updating any user, including themselves
 	if sessionUser.RoleID == data.RoleGuest {
-		r.JSON(res, 403, permissionErr)
+		ren.JSON(w, 403, permissionErr)
 		return
 	}
 
 	// Check for an ID parameter
-	pID, ok := mux.Vars(req)["id"]
+	pID, ok := mux.Vars(r)["id"]
 	if !ok {
-		r.JSON(res, 400, errRes(400, "no integer user ID provided"))
+		ren.JSON(w, 400, errRes(400, "no integer user ID provided"))
 		return
 	}
 
 	// Verify valid integer ID
 	id, err := strconv.Atoi(pID)
 	if err != nil {
-		r.JSON(res, 400, errRes(400, "invalid integer user ID"))
+		ren.JSON(w, 400, errRes(400, "invalid integer user ID"))
 		return
 	}
 
 	// Load the user
-	user := new(data.User)
-	user.ID = id
+	user := &data.User{ID: id}
 	if err := user.Load(); err != nil {
 		// Check for invalid ID
 		if err == sql.ErrNoRows {
-			r.JSON(res, 404, errRes(404, "user ID not found"))
+			ren.JSON(w, 404, errRes(404, "user ID not found"))
 			return
 		}
 
 		// All other errors
 		log.Println(err)
-		r.JSON(res, 500, serverErr)
+		ren.JSON(w, 500, serverErr)
 		return
 	}
 
 	// Check for parameters to update the user
-	if username := req.PostFormValue("username"); username != "" {
+	if username := r.PostFormValue("username"); username != "" {
 		user.Username = username
 	}
 
-	if password := req.PostFormValue("password"); password != "" {
+	if password := r.PostFormValue("password"); password != "" {
 		user.SetPassword(password)
 	}
 
 	// Check for role ID
-	if role := req.PostFormValue("role"); role != "" {
+	if role := r.PostFormValue("role"); role != "" {
 		// Ensure role is valid integer, and valid role
 		roleID, err := strconv.Atoi(role)
 		if err != nil || (roleID != data.RoleGuest && roleID != data.RoleUser && roleID != data.RoleAdmin) {
-			r.JSON(res, 400, errRes(400, "invalid integer role ID"))
+			ren.JSON(w, 400, errRes(400, "invalid integer role ID"))
 			return
 		}
 		user.RoleID = roleID
 
 		// If the user is updating itself and not an administrator, do not allow a role change
 		if sessionUser.RoleID != data.RoleAdmin && sessionUser.RoleID != user.RoleID {
-			r.JSON(res, 403, permissionErr)
+			ren.JSON(w, 403, permissionErr)
 			return
 		}
 	}
 
 	// Only allow administrators to update users, unless the user is updating itself
 	if sessionUser.RoleID < data.RoleAdmin && sessionUser.ID != user.ID {
-		r.JSON(res, 403, permissionErr)
+		ren.JSON(w, 403, permissionErr)
 		return
 	}
 
 	// Save and update the user
 	if err := user.Update(); err != nil {
 		log.Println(err)
-		r.JSON(res, 500, serverErr)
+		ren.JSON(w, 500, serverErr)
 		return
 	}
 
-	// Build response
-	out.Error = nil
-	out.Users = append(out.Users, *user)
-
 	// HTTP 200 OK with JSON
-	r.JSON(res, 200, out)
+	out.Users = []data.User{*user}
+	ren.JSON(w, 200, out)
 	return
 }
 
-// DeleteUsers deletes users from wavepipe, and returns a HTTP status and JSON
-func DeleteUsers(res http.ResponseWriter, req *http.Request) {
+// DeleteUsers deletes users from the wavepipe API, and returns a HTTP status and JSON.
+func DeleteUsers(w http.ResponseWriter, r *http.Request) {
 	// Retrieve render
-	r := context.Get(req, CtxRender).(*render.Render)
+	ren := context.Get(r, CtxRender).(*render.Render)
 
 	// Attempt to retrieve user from context
 	user := new(data.User)
-	if tempUser := context.Get(req, CtxUser); tempUser != nil {
+	if tempUser := context.Get(r, CtxUser); tempUser != nil {
 		user = tempUser.(*data.User)
 	} else {
 		// No user stored in context
 		log.Println("api: no user stored in request context!")
-		r.JSON(res, 500, serverErr)
+		ren.JSON(w, 500, serverErr)
 		return
 	}
 
@@ -298,53 +284,52 @@ func DeleteUsers(res http.ResponseWriter, req *http.Request) {
 	out := UsersResponse{}
 
 	// Check API version
-	if version, ok := mux.Vars(req)["version"]; ok {
+	if version, ok := mux.Vars(r)["version"]; ok {
 		// Check if this API call is supported in the advertised version
 		if !apiVersionSet.Has(version) {
-			r.JSON(res, 400, errRes(400, "unsupported API version: "+version))
+			ren.JSON(w, 400, errRes(400, "unsupported API version: "+version))
 			return
 		}
 	}
 
 	// Only allow administrators to delete users
 	if user.RoleID < data.RoleAdmin {
-		r.JSON(res, 403, permissionErr)
+		ren.JSON(w, 403, permissionErr)
 		return
 	}
 
 	// Check for an ID parameter
-	pID, ok := mux.Vars(req)["id"]
+	pID, ok := mux.Vars(r)["id"]
 	if !ok {
-		r.JSON(res, 400, errRes(400, "no integer user ID provided"))
+		ren.JSON(w, 400, errRes(400, "no integer user ID provided"))
 		return
 	}
 
 	// Verify valid integer ID
 	id, err := strconv.Atoi(pID)
 	if err != nil {
-		r.JSON(res, 400, errRes(400, "invalid integer user ID"))
+		ren.JSON(w, 400, errRes(400, "invalid integer user ID"))
 		return
 	}
 
 	// Load the user
-	delUser := new(data.User)
-	delUser.ID = id
+	delUser := &data.User{ID: id}
 	if err := delUser.Load(); err != nil {
 		// Check for invalid ID
 		if err == sql.ErrNoRows {
-			r.JSON(res, 404, errRes(404, "user ID not found"))
+			ren.JSON(w, 404, errRes(404, "user ID not found"))
 			return
 		}
 
 		// All other errors
 		log.Println(err)
-		r.JSON(res, 500, serverErr)
+		ren.JSON(w, 500, serverErr)
 		return
 	}
 
 	// Verify user is not attempting to delete itself
 	if user.ID == delUser.ID {
-		r.JSON(res, 403, errRes(403, "cannot delete current user"))
+		ren.JSON(w, 403, errRes(403, "cannot delete current user"))
 		return
 	}
 
@@ -352,7 +337,7 @@ func DeleteUsers(res http.ResponseWriter, req *http.Request) {
 	sessions, err := data.DB.SessionsForUser(delUser.ID)
 	if err != nil {
 		log.Println(err)
-		r.JSON(res, 500, serverErr)
+		ren.JSON(w, 500, serverErr)
 		return
 	}
 
@@ -361,7 +346,7 @@ func DeleteUsers(res http.ResponseWriter, req *http.Request) {
 		// Delete the session
 		if err := s.Delete(); err != nil {
 			log.Println(err)
-			r.JSON(res, 500, serverErr)
+			ren.JSON(w, 500, serverErr)
 			return
 		}
 	}
@@ -369,15 +354,12 @@ func DeleteUsers(res http.ResponseWriter, req *http.Request) {
 	// Delete the user
 	if err := delUser.Delete(); err != nil {
 		log.Println(err)
-		r.JSON(res, 500, serverErr)
+		ren.JSON(w, 500, serverErr)
 		return
 	}
 
-	// Build response
-	out.Error = nil
-	out.Users = append(out.Users, *delUser)
-
 	// HTTP 200 OK with JSON
-	r.JSON(res, 200, out)
+	out.Users = []data.User{*user}
+	ren.JSON(w, 200, out)
 	return
 }
