131c13677f46b3472407854671f333d2c339523b
Matt Layher <mdlayher@gmail.com>
2014-08-03 15:02:41 -0400

    api/stream: cleanup

--- api/stream.go
+++ api/stream.go
@@ -17,49 +17,48 @@ import (
 
 // GetStream a raw, non-transcoded, media file stream from wavepipe.  On success, this API will
 // return a binary stream. On failure, it will return a JSON error.
-func GetStream(res http.ResponseWriter, req *http.Request) {
+func GetStream(w http.ResponseWriter, r *http.Request) {
 	// Retrieve render
-	r := context.Get(req, CtxRender).(*render.Render)
+	ren := context.Get(r, CtxRender).(*render.Render)
 
 	// Advertise that clients may send Range requests
-	res.Header().Set("Accept-Ranges", "bytes")
+	w.Header().Set("Accept-Ranges", "bytes")
 
 	// Check API version
-	if version, ok := mux.Vars(req)["version"]; ok {
+	if version, ok := mux.Vars(r)["version"]; ok {
 		// Check if this API call is supported in the advertised version
 		if !apiVersionSet.Has(version) {
-			r.JSON(res, 400, errRes(400, "unsupported API version: "+version))
+			ren.JSON(w, 400, errRes(400, "unsupported API version: "+version))
 			return
 		}
 	}
 
 	// Check for an ID parameter
-	pID, ok := mux.Vars(req)["id"]
+	pID, ok := mux.Vars(r)["id"]
 	if !ok {
-		r.JSON(res, 400, errRes(400, "no integer stream ID provided"))
+		ren.JSON(w, 400, errRes(400, "no integer stream ID provided"))
 		return
 	}
 
 	// Verify valid integer ID
 	id, err := strconv.Atoi(pID)
 	if err != nil {
-		r.JSON(res, 400, errRes(400, "invalid integer stream ID"))
+		ren.JSON(w, 400, errRes(400, "invalid integer stream ID"))
 		return
 	}
 
 	// Attempt to load the song with matching ID
-	song := new(data.Song)
-	song.ID = id
+	song := &data.Song{ID: id}
 	if err := song.Load(); err != nil {
 		// Check for invalid ID
 		if err == sql.ErrNoRows {
-			r.JSON(res, 404, errRes(404, "song ID not found"))
+			ren.JSON(w, 404, errRes(404, "song ID not found"))
 			return
 		}
 
 		// All other errors
 		log.Println(err)
-		r.JSON(res, 500, serverErr)
+		ren.JSON(w, 500, serverErr)
 		return
 	}
 
@@ -67,7 +66,7 @@ func GetStream(res http.ResponseWriter, req *http.Request) {
 	stream, err := song.Stream()
 	if err != nil {
 		log.Println(err)
-		r.JSON(res, 500, serverErr)
+		ren.JSON(w, 500, serverErr)
 		return
 	}
 
@@ -79,7 +78,7 @@ func GetStream(res http.ResponseWriter, req *http.Request) {
 	log.Println("stream: starting:", opStr)
 
 	// Pass stream using song's file size, auto-detect MIME type
-	if err := HTTPStream(song, "", song.FileSize, stream, req, res); err != nil {
+	if err := HTTPStream(song, "", song.FileSize, stream, r, w); err != nil {
 		// Check for client reset
 		if strings.Contains(err.Error(), "connection reset by peer") || strings.Contains(err.Error(), "broken pipe") {
 			return
@@ -87,7 +86,7 @@ func GetStream(res http.ResponseWriter, req *http.Request) {
 
 		// Check for invalid range, return HTTP 416
 		if err == ErrInvalidRange {
-			r.JSON(res, 416, errRes(416, "invalid HTTP Range header boundaries"))
+			ren.JSON(w, 416, errRes(416, "invalid HTTP Range header boundaries"))
 			return
 		}
 
