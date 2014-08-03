780f19b4034af1a6c533bec98ca22c6f774451a9
Matt Layher <mdlayher@gmail.com>
2014-08-03 14:54:38 -0400

    api/logout: cleanup

--- api/logout.go
+++ api/logout.go
@@ -11,30 +11,28 @@ import (
 	"github.com/unrolled/render"
 )
 
-// PostLogout destroys an existing session from the wavepipe API, and returns a HTTP status and JSON
-func PostLogout(res http.ResponseWriter, req *http.Request) {
+// PostLogout destroys an existing session from the wavepipe API,
+// and returns a HTTP status and JSON.
+func PostLogout(w http.ResponseWriter, r *http.Request) {
 	// Retrieve render
-	r := context.Get(req, CtxRender).(*render.Render)
+	ren := context.Get(r, CtxRender).(*render.Render)
 
 	// Attempt to retrieve session from context
 	session := new(data.Session)
-	if tempSession := context.Get(req, CtxSession); tempSession != nil {
+	if tempSession := context.Get(r, CtxSession); tempSession != nil {
 		session = tempSession.(*data.Session)
 	} else {
 		// No session stored in context
 		log.Println("api: no session stored in request context!")
-		r.JSON(res, 500, serverErr)
+		ren.JSON(w, 500, serverErr)
 		return
 	}
 
-	// Output struct for logout request
-	out := ErrorResponse{}
-
 	// Check API version
-	if version, ok := mux.Vars(req)["version"]; ok {
+	if version, ok := mux.Vars(r)["version"]; ok {
 		// Check if this API call is supported in the advertised version
 		if !apiVersionSet.Has(version) {
-			r.JSON(res, 400, errRes(400, "unsupported API version: "+version))
+			ren.JSON(w, 400, errRes(400, "unsupported API version: "+version))
 			return
 		}
 	}
@@ -43,15 +41,12 @@ func PostLogout(res http.ResponseWriter, req *http.Request) {
 	if session != nil {
 		if err := session.Delete(); err != nil {
 			log.Println(err)
-			r.JSON(res, 500, serverErr)
+			ren.JSON(w, 500, serverErr)
 			return
 		}
 	}
 
-	// No errors
-	out.Error = nil
-
 	// HTTP 200 OK with JSON
-	r.JSON(res, 200, out)
+	ren.JSON(w, 200, ErrorResponse{})
 	return
 }
